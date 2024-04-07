e667f3f6fec01b0123849f26215488bc8832185a
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2024-04-07 23:13:12 +0700

    add cam create anchor and positive

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ print(' ')
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
-    print(gpu.name)
+    print('current gpu: ', gpu)
