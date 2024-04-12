d19267e5fa76f87d81fac3030a0fc9f5a3bf1c43
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2024-04-12 07:57:56 +0700

    add trained verify face model

---
+++ test_model.py
@@ -0,0 +1,99 @@
+# Import standard dependencies
+import cv2
+import os
+import random
+import numpy as np
+from matplotlib import pyplot as plt
+# Import tensorflow dependencies - Functional API
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
+import tensorflow as tf
+import os
+import uuid
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print('gpus: ', gpus)
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+    print('cur gpu: ', gpu)
+
+POS_PATH = os.path.join('data', 'positive')
+NEG_PATH = os.path.join('data', 'negative')
+ANC_PATH = os.path.join('data', 'anchor')
+
+
+class L1Dist(Layer):
+
+    # Init method - inheritance
+    def __init__(self, **kwargs):
+        super().__init__()
+
+    # Magic happens here - similarity calculation
+    # def call(self, input_embedding, validation_embedding):
+    def call(self, inputs):
+        # return tf.math.abs(input_embedding - validation_embedding)
+        input_embedding, validation_embedding = inputs
+        return tf.math.abs(input_embedding - validation_embedding)
+
+
+model = tf.keras.models.load_model('siamesemodelv2.keras', custom_objects={'L1Dist': L1Dist})
+
+model.summary()
+
+
+def preprocess(file_path):
+    # Read in image from file path
+    byte_img = tf.io.read_file(file_path)
+    # Load in the image
+    img = tf.io.decode_jpeg(byte_img)
+
+    # Preprocessing steps - resizing the image to be 100x100x3
+    img = tf.image.resize(img, (100, 100))
+    # Scale image to be between 0 and 1
+    img = img / 255.0
+
+    # Return image
+    return img
+
+
+def verify(model, detection_threshold, verification_threshold):
+    # Build results array
+    results = []
+    for image in os.listdir(os.path.join('application_data', 'verification_images')):
+        input_img = preprocess(os.path.join('application_data', 'input_image', 'input_image.jpg'))
+        validation_img = preprocess(os.path.join('application_data', 'verification_images', image))
+
+        # Make Predictions
+        result = model.predict(list(np.expand_dims([input_img, validation_img], axis=1)))
+        results.append(result)
+
+    # Detection Threshold: Metric above which a prediciton is considered positive
+    detection = np.sum(np.array(results) > detection_threshold)
+
+    # Verification Threshold: Proportion of positive predictions / total positive samples
+    verification = detection / len(os.listdir(os.path.join('application_data', 'verification_images')))
+    verified = verification > verification_threshold
+
+    return results, verified
+
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    frame = frame[120:120+250,200:200+250, :]
+
+    cv2.imshow('Verification', frame)
+
+    # Verification triggerv
+    if cv2.waitKey(10) & 0xFF == ord('v'):
+        # Save input image to application_data/input_image folder
+        cv2.imwrite(os.path.join('application_data', 'input_image', 'input_image.jpg'), frame)
+
+        # Run verification
+        results, verified = verify(model, 0.9, 0.7)
+        print(verified)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
