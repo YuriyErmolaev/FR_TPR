3beb542940122ccd0a0d1531f62f4130b68d7612
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2024-04-20 01:00:42 +0700

    format main

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 from setup import setup_gpu, setup_paths
 setup_gpu()
-#step 1
-# Setup GPU and data paths
-# setup.py # move into top for avoid errors
-
+#step 1 # Setup GPU and data paths # setup.py # move into top for avoid errors
 from data_loading import load_and_count_images, pair_and_label_images, preprocess #preprocess use in data pipeline
 from data_pipeline import configure_training_pipeline, split_data
 from model_architecture import make_embedding, make_siamese_model
 from training import train
 
-
-
 POS_PATH, NEG_PATH, ANC_PATH = setup_paths()
-
 
 # Step 2, 3, 4
 # Load dataset images, preprocess, and label pairs for training
 # data_loading.py
 anchor, positive, negative = load_and_count_images(ANC_PATH, POS_PATH, NEG_PATH)
 data = pair_and_label_images(anchor, positive, negative)
-
 
 # Step 5 to 8
 # Preprocess paired images, configure the training pipeline, and split data sets
@@ -36,7 +29,6 @@ embedding.summary()
 #step 11 # Def custom layer to calc L1 distance for Siamese network # model_architecture.py
 #step 12 # def Construct Siamese network model # model_architecture.py
 
-
 #step 13
 # Init and show Siam network
 siamese_model = make_siamese_model()
@@ -47,18 +39,11 @@ siamese_model.summary()
 #step 16 # Def train step for Siamese model # training.py
 #step 17 # Def train process over epochs # training.py
 
-
 #step 18
 # Exec train with number epochs
-
 EPOCHS = 50
-# train(train_data, EPOCHS)
 train(train_data, siamese_model, EPOCHS)
-
-
 
 #step 19
 #save model to file
-
-
 siamese_model.save('siamesemodelv3.keras')
